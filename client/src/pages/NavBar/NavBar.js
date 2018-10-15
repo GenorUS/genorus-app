a9b04b0b893a6c779c41421b6d533ce44f9b9aa2
Zach Wiest <zachwiest@Zachs-MBP-3.fios-router.home>
2018-10-15 12:19:56 -0400

    Trying to get navbar icons up

--- client/src/pages/NavBar/NavBar.js
+++ client/src/pages/NavBar/NavBar.js
@@ -51,7 +51,7 @@ class NavBar extends Component {
           <Div className="collapse navbar-collapse" id="navbarResponsive">
               <UL className="navbar-nav ml-auto">
                   <LI className="nav-item">
-                    <A className="navbar-brand" href="/" src="../../../assets/images/logospins/tinylogospin.gif" alt="tinylogospin" width="50px" height="50px">About Genorus</A>
+                    <A className="navbar-brand" href="/">About Genorus</A>
                   </LI>
                   <LI className="nav-item">
                     <A className="navbar-brand" href="/contact">Contact</A>
