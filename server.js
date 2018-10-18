aa0578009098bbf5e0adaa5d66d00cb7e85f241a
smibbie <35998407+smibbie@users.noreply.github.com>
2018-10-18 18:52:17 -0400

    set up server.js

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
+// Dependencies ------------------------------------
 const express = require("express");
-
-const mongoose = require("mongoose");
 const routes = require("./routes");
+
+// Express and PORT ------------------------------------
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Define middleware here
+// Define middleware here --------------------------
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
+
+// Serve up static assets (usually on heroku) ------
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Add routes, both API and view
+// Add routes, both API and view --------------------
 app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytreact");
-
-// Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+// Start the API server and DB
+db.sequelize.sync().then(() => {
+  app.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(`Connected on localhost:${PORT}`);
+  });
 });
