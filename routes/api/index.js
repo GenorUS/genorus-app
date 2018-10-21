4570b9e1b085e3c479fba2564c3fff8a307d41ba
amh5366 <amh5366@upenn.edu>
2018-10-20 22:59:10 -0400

    merge conflict

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,19 +1,11 @@
 // Dependencies -----------------------------------------
 const router = require("express").Router();
-<<<<<<< HEAD
-=======
-
->>>>>>> 7ef49fe4b63dd68db429aeafe8575fa40075330a
 const application = require("./application");
 const user = require("./user");
 
 // Defining Routes to use -------------------------------
 router.use("/application", application);
 router.use("/users", user);
-<<<<<<< HEAD
-=======
-
->>>>>>> 7ef49fe4b63dd68db429aeafe8575fa40075330a
 
 // Export ------------------------------------------------
 module.exports = router;
