df046fcfc43cbe0185343c34f1666269c5e01d94
smibbie <35998407+smibbie@users.noreply.github.com>
2018-10-21 20:06:44 -0400

    changed routing for company pages

--- client/src/App.js
+++ client/src/App.js
@@ -6,34 +6,16 @@ import Contact from "./pages/Contact";
 import CompanyPages from "./pages/CompanyPages";
 import {SignIn, SignUp} from "./pages/SignIn";
 
-// import Detail from "./pages/Detail";
-// import NoMatch from "./pages/NoMatch";
-// import Nav from "./components/Nav";
-
-// const App = () => (
-//   <Router>
-//     <div>
-//       <Nav />
-//       <Switch>
-//         <Route exact path="/" component={Books} />
-//         <Route exact path="/books" component={Books} />
-//         <Route exact path="/books/:id" component={Detail} />
-//         <Route component={NoMatch} />
-//       </Switch>
-//     </div>
-//   </Router>
-// );
-
 const App = () => {
   return (
     <Router>
       <div>
         <Route exact path="/" component={Home} />
         <Route exact path="/scholarships" component={Portfolio} />
+        <Route path="/scholarships/:company" component={CompanyPages}/>
         <Route exact path="/contact" component={Contact} />
         <Route exact path="/signin" component={SignIn} />
         <Route exact path="/signup" component={SignUp} />
-        <Route exact path="/company/:id" component={CompanyPages} />
       </div>
     </Router>
   );
